c4f2171d22dfe75f4fb4079a3eaf3179ef6f3f13
João Gouveia <105375581+SkyWasAlive@users.noreply.github.com>
2024-04-15 23:51:54 +0100

    Added netcode classes but not implementation

---
+++ Runtime/Netcode/GameClient.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UFramework.Netcode
+{
+    public class GameClient : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Runtime/Netcode/NetworkObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UFramework.Netcode
+{
+    public class NetworkObject : MonoBehaviour
+    {
+
+    }
+}
