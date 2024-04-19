3bbb3c7df33fbb439649ac8dc4b439babae06bcc
João Gouveia <joao.c.gouveia10@gmail.com>
2024-04-19 18:07:36 +0100

    Update GameEvent.cs

--- Runtime/Events/GameEvent.cs
+++ Runtime/Events/GameEvent.cs
@@ -9,11 +9,13 @@ namespace UFramework.GameEvents
     {
         private List<GameEventListener<T>> _listeners = new List<GameEventListener<T>>();
         
+        /*
         public void Raise()
         {
             for(int i = _listeners.Count -1; i >= 0; i--)
                 _listeners[i].OnEventRaised(null);
         }
+        */
 
         public void Raise(T eventParameter){
             for(int i = _listeners.Count -1; i >= 0; i--)
